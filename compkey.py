8ae3f54d6d0b35408d1c1374e4480f1a93d90de1
moke <2061610579@qq.com>
2023-11-08 11:14:52 +0800

    Revert "第二部分完成"
    This reverts commit d9bbf1c0

--- compkey.py
+++ compkey.py
@@ -1,14 +1,17 @@
 import os
-import sys
 from pypinyin import lazy_pinyin
-import mysql.connector
+
 
 def calculate_competitive_keywords(tfidf_scores):
+    # 根据TF-IDF分数排序关键词
     sorted_keywords = sorted(tfidf_scores.items(), key=lambda item: item[1], reverse=True)
     return sorted_keywords
 
+
 def read_tfidf_scores(output_dir, keyword):
+    # 将关键词转换为拼音
     keyword_pinyin = ''.join(lazy_pinyin(keyword))
+    # 根据新的目录和文件命名规则构建文件路径
     output_file_path = os.path.join(output_dir, f'tfidf_{keyword_pinyin}.txt')
 
     tfidf_scores = {}
@@ -18,50 +21,36 @@ def read_tfidf_scores(output_dir, keyword):
             if len(parts) == 2:
                 word, score = parts
                 tfidf_scores[word] = float(score)
+
     return tfidf_scores
 
-def get_db_config(filename):
-    config = {}
-    with open(filename, 'r', encoding="utf-8") as file:
-        for line in file:
-            if line.startswith('#') or '=' not in line:
-                continue
-            key, value = line.strip().split('=', 1)
-            config[key.strip()] = value.strip()
-    return config
 
-def write_competitive_keywords_to_db(db_config, keyword, competitive_keywords):
-    conn = mysql.connector.connect(
-        host=db_config['db_host'],
-        user=db_config['db_user'],
-        password=db_config['db_password'],
-        database=db_config['db_name']
-    )
-    cursor = conn.cursor()
+def write_competitive_keywords(output_dir, keyword, competitive_keywords):
+    # 将关键词转换为拼音
+    keyword_pinyin = ''.join(lazy_pinyin(keyword))
+    output_file_path = os.path.join(output_dir, f'compete_{keyword_pinyin}.txt')
 
-    cursor.execute("INSERT INTO SeedKeyword (Keyword) VALUES (%s)", (keyword,))
-    seed_keyword_id = cursor.lastrowid
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        for word, weight in competitive_keywords:
+            file.write(f"{word}: {weight}\n")
 
-    for word, weight in competitive_keywords:
-        cursor.execute("INSERT INTO CompetitiveKeyword (SeedKeywordID, Keyword, CompetitiveScore) VALUES (%s, %s, %s)",
-                       (seed_keyword_id, word, weight))
-
-    conn.commit()
-    cursor.close()
-    conn.close()
 
 # 设置文件夹路径
 tfidf_output_dir = 'tfidf_output'  # 修改为正确的输入目录
 
-# 读取数据库配置
-db_config = get_db_config('config.ini')
+# 确保compete_output目录存在
+compete_output_dir = 'compete_output'
+if not os.path.exists(compete_output_dir):
+    os.makedirs(compete_output_dir)
 
 # 关键词列表
-keywords = sys.argv[1:]
+keywords = ['郭德纲', '女']
 
-# 主要逻辑
+# 对于列表中的每个关键词读取TF-IDF权重并计算竞争性关键词
 for keyword in keywords:
     tfidf_scores = read_tfidf_scores(tfidf_output_dir, keyword)
     competitive_keywords = calculate_competitive_keywords(tfidf_scores)
-    write_competitive_keywords_to_db(db_config, keyword, competitive_keywords)
-    print(f"关键词 '{keyword}' 的竞争性关键词及权重已写入数据库")
+    write_competitive_keywords(compete_output_dir, keyword, competitive_keywords)
+
+    # 打印结果的路径
+    print(f"关键词 '{keyword}' 的竞争性关键词及权重已写入: {compete_output_dir}/compete_{''.join(lazy_pinyin(keyword))}.txt")
